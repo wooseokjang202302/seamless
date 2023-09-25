e9c382750faf429dd904659c24e898743319a065
wooseok <william741@naver.com>
2023-09-25 18:19:33 +0900

    Initial commit

---
+++ src/main/java/com/seamless/SeamlessApplication.java
@@ -0,0 +1,13 @@
+package com.seamless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamlessApplication.class, args);
+	}
+
+}
