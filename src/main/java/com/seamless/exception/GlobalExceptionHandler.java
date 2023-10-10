214e073a97c4e6795135af33bd5fa609e13b094e
wooseok jang <william741@naver.com>
2023-10-10 18:18:49 +0900

    중복 예외처리제거, 예외처리 json형식으로 응답할 수 있게 수정

--- src/main/java/com/seamless/exception/GlobalExceptionHandler.java
+++ src/main/java/com/seamless/exception/GlobalExceptionHandler.java
@@ -2,52 +2,38 @@ package com.seamless.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.security.sasl.AuthenticationException;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    // DTO 유효성 검사 실패 시 예외 처리
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        BindingResult bindingResult = ex.getBindingResult();
-        bindingResult.getFieldErrors().forEach(error ->
-                errors.put(error.getField(), error.getDefaultMessage()));
-        return errors;
-    }
-
     // 중복 회원 예외 처리
     @ExceptionHandler(IllegalStateException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalStateException(IllegalStateException ex) {
-        return ex.getMessage();
+    public ErrorResponse handleIllegalStateException(IllegalStateException ex) {
+        return new ErrorResponse(ex.getMessage());
     }
 
+    // 확인용 비밀번호 예외 처리
     @ExceptionHandler(IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ex.getMessage();
+    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ErrorResponse(ex.getMessage());
     }
 
     // 인증 실패 예외 처리
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public String handleAuthenticationException(AuthenticationException ex) {
-        return ex.getMessage();
+    public ErrorResponse handleAuthenticationException(AuthenticationException ex) {
+        return new ErrorResponse(ex.getMessage());
     }
 
     // 그 외 일반적인 예외 처리
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예기치 못한 에러가 발생했습니다 : " + ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("예기치 못한 에러가 발생했습니다 : " + ex.getMessage()));
     }
 }
